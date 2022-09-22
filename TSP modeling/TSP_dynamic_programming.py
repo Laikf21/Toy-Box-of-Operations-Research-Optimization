28328bd2343445409bb31387254588b055d8c4b2
Kefan Lai <laikf21@mails.tsinghua.edu.cn>
2022-09-22 11:10:04 +0800

    update

--- TSP modeling/TSP_dynamic_programming.py
+++ TSP modeling/TSP_dynamic_programming.py
@@ -1,11 +1,8 @@
-
-
-
-
-
+'''
+The program is problematic, need to be debugged. 
+'''
 
 import copy
-from lib2to3.pytree import Node
 import re
 import math
 import itertools
@@ -17,9 +14,11 @@ Nodes = [1, 2, 3, 4]
 nodeNum = len(Nodes)
 
 dis_matrix = np.array([[0, 8, 12, 17]
-                    , [4, 0, 10, 9]
-                    , [7, 14, 0, 13]
-                    , [9, 7, 11, 0]])
+                     ,[4, 0, 10, 9]
+                     ,[7, 14, 0, 13]
+                     ,[9, 7, 11, 0]])
+
+
 
 def TSP_Dynamic_Programming(Nodes, dis_matrix):
     Labet_set = {}
@@ -29,7 +28,8 @@ def TSP_Dynamic_Programming(Nodes, dis_matrix):
     # cycle stage: V, V-1, ..., 2
     for stage_ID in range(nodeNum, 1, -1):
         print('stage :', stage_ID)
-        for i in range(2, nodeNum+1):
+
+        for i in range( 2, nodeNum + 1 ):
             current_node = i
             left_node_list = copy.deepcopy(Nodes)
             if (org in left_node_list):
@@ -37,11 +37,11 @@ def TSP_Dynamic_Programming(Nodes, dis_matrix):
             left_node_set = set(left_node_list)
 
             #obtain all the subset of left node set
-            subset_all = list( map(set, itertools.combinations(left_node_set, nodeNum - stage_ID)) )
+            subset_all = list(map(set, itertools.combinations(left_node_set, nodeNum - stage_ID)) )
 
             for subset in subset_all:
                 if(len(subset) == 0):
-                    key = (stage_ID, current_node, "None")
+                    key = (stage_ID, current_node, 'None')
                     next_node = org
                     Labet_set[key] = [dis_matrix[i-1][org-1], next_node]
                 else:
@@ -52,7 +52,9 @@ def TSP_Dynamic_Programming(Nodes, dis_matrix):
                         subsub_set.remove(temp_next_node)
                         if(subsub_set == None or len(subsub_set) == 0):
                             subsub_set = 'None'
+
                         sub_key = (stage_ID +1, temp_next_node, str(subsub_set))
+
                         if (sub_key in Labet_set.keys()):
                             if(dis_matrix[current_node - 1][temp_next_node-1] + Labet_set[sub_key][0] < min_distance):
                                 min_distance = dis_matrix[current_node - 1][temp_next_node-1]+Labet_set[sub_key][0]
@@ -71,9 +73,9 @@ def TSP_Dynamic_Programming(Nodes, dis_matrix):
         subsub_set.remove(temp_next_node)
         if(subsub_set == None or len(subsub_set) == 0):
             subsub_set = 'None'
-        sub_key = (stage_ID +1, temp_next_node,str(subsub_set))
+        sub_key = (stage_ID +1, temp_next_node, str(subsub_set))
         if(sub_key in Labet_set.keys()):
-            if(dis_matrix[current_node-1][temp_next_node-1] + Labet_set[sub_key][0]<min_distance):
+            if(dis_matrix[current_node-1][temp_next_node-1] + Labet_set[sub_key][0] < min_distance):
                 min_distance = dis_matrix[current_node-1][temp_next_node-1]+Labet_set[sub_key][0]
                 next_node = temp_next_node
                 Labet_set[final_key] = [min_distance, next_node]
@@ -83,15 +85,21 @@ def TSP_Dynamic_Programming(Nodes, dis_matrix):
     not_visited_node = set(copy.deepcopy(Nodes))
     not_visited_node.remove(org)
     next_node = Labet_set[final_key][1]
-    while True:
+    
+    # print(final_key)
+    # print(Labet_set)
+
+    while(True):
         opt_route.append(next_node)
-        if( len(opt_route) == nodeNum +1):
+        if( len(opt_route) == nodeNum +1 ):
             break
         current_stage = len(opt_route)
         not_visited_node.remove(next_node)
-        if( not_visited_node == None or len(not_visited_node)==0 ):
+        if( not_visited_node == None or len(not_visited_node) == 0 ):
             not_visited_node = 'None'
         next_key = (current_stage, next_node, str(not_visited_node))
+        print(next_key)
+        print(Labet_set)
         next_node = Labet_set[next_key][1]
 
     opt_dis = Labet_set[final_key][0]
@@ -101,8 +109,11 @@ def TSP_Dynamic_Programming(Nodes, dis_matrix):
 
     return opt_dis, opt_route
 
+
 # call DP function to solve TSP
 opt_dis, opt_route = TSP_Dynamic_Programming(Nodes, dis_matrix)
 print('\n\n -------------- optimal solution --------------\n')
 print('objevtive:', opt_dis)
 print('optimal rounte:', opt_route)
+
+
